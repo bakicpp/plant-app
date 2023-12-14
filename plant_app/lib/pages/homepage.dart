bb5f0dc447363a43442331992bd20ab5581030e0
bakicpp <bakiyumak05@gmail.com>
2023-12-14 22:47:51 +0100

    firebase issue fix

--- plant_app/lib/pages/homepage.dart
+++ plant_app/lib/pages/homepage.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:plant_app/firebase_services/database_service.dart';
 
 class Homepage extends StatefulWidget {
   const Homepage({super.key});
@@ -10,22 +11,34 @@ class Homepage extends StatefulWidget {
 class _HomepageState extends State<Homepage> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       body: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         crossAxisAlignment: CrossAxisAlignment.center,
         children: [
-          Center(child: Text("Hello World")),
-          Padding(
-            padding: EdgeInsets.all(18.0),
-            child: TextField(
-              decoration: InputDecoration(
-                border: OutlineInputBorder(),
-                labelText: 'Enter your username',
-              ),
-            ),
-          )
+          const Center(child: Text("Hello World")),
+          textField(),
+          button(),
         ],
+      ),
+    );
+  }
+
+  ElevatedButton button() => ElevatedButton(
+      onPressed: () {
+        FirebaseCollection("users").addDocument({"name": "John Doe"});
+        print("added data");
+      },
+      child: const Text("add data"));
+
+  Padding textField() {
+    return const Padding(
+      padding: EdgeInsets.all(18.0),
+      child: TextField(
+        decoration: InputDecoration(
+          border: OutlineInputBorder(),
+          labelText: 'Enter your username',
+        ),
       ),
     );
   }
